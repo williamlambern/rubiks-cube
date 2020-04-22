86c9597aad857ef0eeaf6421bd2545fe16bd4f7d
williamlambern <47496042+williamlambern@users.noreply.github.com>
2020-04-22 09:37:27 +0100

    Add files via upload

---
+++ src/cube.py
@@ -0,0 +1,233 @@
+#centre of cube i,j,k 0,0,0
+
+'''
+to add:
+- solve detection
+- ai
+'''
+
+from rotation_matrices import *
+
+
+class Cube:
+    
+    def __init__(self):
+        self.data = []
+        self.sorted = [[],[],[]]
+        self.reset()
+
+    def reset(self):
+        
+        #front
+        self.data.append(Piece(-1,1,1, ['b','r','w']))
+        self.data.append(Piece(0,1,1, [None,'r','w']))
+        self.data.append(Piece(1,1,1, ['g','r','w']))
+
+        self.data.append(Piece(-1,0,1, ['b',None,'w']))
+        self.data.append(Piece(0,0,1, [None,None,'w']))
+        self.data.append(Piece(1,0,1, ['g',None,'w']))
+
+        self.data.append(Piece(-1,-1,1, ['b','o','w']))
+        self.data.append(Piece(0,-1,1, [None,'o','w']))
+        self.data.append(Piece(1,-1,1, ['g','o','w']))
+
+        #middle
+        self.data.append(Piece(-1,1,0, ['b','r',None]))
+        self.data.append(Piece(0,1,0, [None,'r',None]))
+        self.data.append(Piece(1,1,0, ['g','r',None]))
+
+        self.data.append(Piece(-1,0,0, ['b',None,None]))
+        self.data.append(Piece(0,0,0, [None,None,None])) #centre piece
+        self.data.append(Piece(1,0,0, ['g',None,None]))
+        
+        self.data.append(Piece(-1,-1,0, ['b','o',None]))
+        self.data.append(Piece(0,-1,0, [None,'o',None]))
+        self.data.append(Piece(1,-1,0, ['g','o',None]))
+
+        #back
+        self.data.append(Piece(-1,1,-1, ['b','r','y']))
+        self.data.append(Piece(0,1,-1, [None,'r','y']))
+        self.data.append(Piece(1,1,-1, ['g','r','y']))
+        
+        self.data.append(Piece(-1,0,-1, ['b',None,'y']))
+        self.data.append(Piece(0,0,-1, [None,None,'y']))
+        self.data.append(Piece(1,0,-1, ['g',None,'y']))
+
+        self.data.append(Piece(-1,-1,-1, ['b','o','y']))
+        self.data.append(Piece(0,-1,-1, [None,'o','y']))
+        self.data.append(Piece(1,-1,-1, ['g','o','y']))
+
+        self.sort() #store in a logical way. 3d list with indexes [i][j][k] to find pieces quicker
+
+    def sort(self):
+        self.sorted = {}
+        for p in range(len(self.data)):
+            self.sorted[str(str(self.data[p].i) + str(self.data[p].j) + str(self.data[p].k))] = self.data[p]
+
+
+    def rotate(self,fr):
+        fr = fr.lower()
+        transformation_vectors = {'l' : l, "l'" : l_prime, 'r' : r, "r'" : r_prime, 'f' : f, "f'" : f_prime, 'b' : b, "b'" : b_prime, 'u' : u, "u'" : u_prime, 'd' : d, "d'" : d_prime}
+        
+        if fr == 'l' or fr == "l'":
+            for p in range(len(self.data)):
+                if self.data[p].i == -1: #x=-1 (left face)
+                    position = [self.data[p].i,self.data[p].j,self.data[p].k]
+                    new_position = self.matrix_multiplication(transformation_vectors[fr],position)
+                    
+                    self.data[p].i = new_position[0]
+                    self.data[p].j = new_position[1]
+                    self.data[p].k = new_position[2]
+
+                    colours_before = self.data[p].colour
+                    colours_after = [colours_before[0], colours_before[2], colours_before[1]]
+                    self.data[p].colour = colours_after
+
+        if fr == 'r' or fr == "r'":
+            for p in range(len(self.data)):
+                if self.data[p].i == 1: #x=1 (right face)
+                    position = [self.data[p].i,self.data[p].j,self.data[p].k]
+                    new_position = self.matrix_multiplication(transformation_vectors[fr],position)
+                    
+                    self.data[p].i = new_position[0]
+                    self.data[p].j = new_position[1]
+                    self.data[p].k = new_position[2]
+
+                    colours_before = self.data[p].colour
+                    colours_after = [colours_before[0], colours_before[2], colours_before[1]]
+                    self.data[p].colour = colours_after
+
+        if fr == 'f' or fr == "f'":
+            for p in range(len(self.data)):
+                if self.data[p].k == 1: #k=-1 (front face)
+                    position = [self.data[p].i,self.data[p].j,self.data[p].k]
+                    new_position = self.matrix_multiplication(transformation_vectors[fr],position)
+                    
+                    self.data[p].i = new_position[0]
+                    self.data[p].j = new_position[1]
+                    self.data[p].k = new_position[2]
+
+                    colours_before = self.data[p].colour
+                    colours_after = [colours_before[1], colours_before[0], colours_before[2]]
+                    self.data[p].colour = colours_after
+
+        if fr == 'b' or fr == "b'":
+            for p in range(len(self.data)):
+                if self.data[p].k == -1: #k=-1 (back face)
+                    position = [self.data[p].i,self.data[p].j,self.data[p].k]
+                    new_position = self.matrix_multiplication(transformation_vectors[fr],position)
+                    
+                    self.data[p].i = new_position[0]
+                    self.data[p].j = new_position[1]
+                    self.data[p].k = new_position[2]
+
+                    colours_before = self.data[p].colour
+                    colours_after = [colours_before[1], colours_before[0], colours_before[2]]
+                    self.data[p].colour = colours_after
+
+        if fr == 'u' or fr == "u'":
+            for p in range(len(self.data)):
+                if self.data[p].j == 1: #j=1 (top face)
+                    position = [self.data[p].i,self.data[p].j,self.data[p].k]
+                    new_position = self.matrix_multiplication(transformation_vectors[fr],position)
+                    
+                    self.data[p].i = new_position[0]
+                    self.data[p].j = new_position[1]
+                    self.data[p].k = new_position[2]
+
+                    colours_before = self.data[p].colour
+                    colours_after = [colours_before[2], colours_before[1], colours_before[0]]
+                    self.data[p].colour = colours_after
+
+        if fr == 'd' or fr == "d'":
+            for p in range(len(self.data)):
+                if self.data[p].j == -1: #j=-1 (bottom face)
+                    position = [self.data[p].i,self.data[p].j,self.data[p].k]
+                    new_position = self.matrix_multiplication(transformation_vectors[fr],position)
+                    
+                    self.data[p].i = new_position[0]
+                    self.data[p].j = new_position[1]
+                    self.data[p].k = new_position[2]
+
+                    colours_before = self.data[p].colour
+                    colours_after = [colours_before[2], colours_before[1], colours_before[0]]
+                    self.data[p].colour = colours_after
+                 
+                    
+        self.sort()
+
+
+    def matrix_multiplication(self,tm,pv):
+        #tm = transformation matrix , pv = position vector
+        nv = []
+        nv.append(tm[0][0]*pv[0] + tm[0][1]*pv[1] + tm[0][2]*pv[2])
+        nv.append(tm[1][0]*pv[0] + tm[1][1]*pv[1] + tm[1][2]*pv[2])
+        nv.append(tm[2][0]*pv[0] + tm[2][1]*pv[1] + tm[2][2]*pv[2])
+
+        return nv
+
+    def isCubeSolved(self):
+        if self.isFaceSolved(x=-1) == True and self.isFaceSolved(x=0) == True:
+            if self.isFaceSolved(x=1) == True:
+                if self.isFaceSolved(y=-1) == True and self.isFaceSolved(y=0) == True:
+                    if self.isFaceSolved(y=1) == True:
+                        if self.isFaceSolved(z=-1) == True and self.isFaceSolved(z=0) == True:
+                            if self.isFaceSolved(z=1) == True:
+                                return True
+        return False
+
+    def isFaceSolved(self, **kwargs):
+        if 'x' in kwargs.keys():
+            value = kwargs['x']
+            colour = 0
+            toCheck = '{0}11 {0}01 {0}-11 {0}10 {0}00 {0}-10 {0}1-1 {0}0-1 {0}-1-1'.format(value).split(' ')
+            
+        elif 'y' in kwargs.keys():
+            value = kwargs['y']
+            colour = 1
+            toCheck = '1{0}1 1{0}0 1{0}-1 0{0}1 0{0}0 0{0}-1 -1{0}1 -1{0}0 -1{0}-1'.format(value).split(' ')
+
+        elif 'z' in kwargs.keys():
+            value = kwargs['z']
+            colour = 2
+            toCheck = '11{0} 10{0} 1-1{0} 01{0} 00{0} 0-1{0} -11{0} -10{0} -1-1{0}'.format(value).split(' ')
+            
+        else:
+            return False #call error
+
+        
+        for i in range(len(toCheck)-1):
+            if self.sorted[toCheck[i]].colour[colour] != self.sorted[toCheck[i+1]].colour[colour]:
+             
+                return False
+        return True
+
+
+    def numberOfPairs(self):
+        pairs = 0
+        for i in range(-1,1):
+            for j in range(-1,1):
+                for k in range(-1,1):
+                    for c in range(3):
+                        if self.sorted['{}{}{}'.format(i,j,k)].colour[c] != None:
+                            if self.sorted['{}{}{}'.format(i,j,k)].colour[c] == self.sorted['{}{}{}'.format(i,j,k+1)].colour[c]:
+                                pairs += 1
+
+                            if self.sorted['{}{}{}'.format(i,j,k)].colour[c] == self.sorted['{}{}{}'.format(i,j+1,k)].colour[c]:
+                                pairs += 1
+
+                            if self.sorted['{}{}{}'.format(i,j,k)].colour[c] == self.sorted['{}{}{}'.format(i+1,j,k)].colour[c]:
+                                pairs += 1
+        return pairs
+
+class Piece:
+
+    def __init__(self,i,j,k,colour):
+        self.i = i
+        self.j = j
+        self.k = k
+        self.colour = colour #list in form [ci,cj,ck}
+
+
+
+
---
+++ src/gui.py
@@ -0,0 +1,157 @@
+from tkinter import Tk, Canvas, Frame, Entry, Button, BOTH
+from cube import *
+from solver import *
+import random
+
+class Window:
+
+    def __init__(self,master):
+        #GUI INIT
+        self.master = master
+        self.master.geometry("400x450")
+        self.canvas = Canvas(self.master)
+        
+        self.e1 = Entry(self.canvas)
+        self.canvas.create_window(100,20,window=self.e1)
+
+        self.button = Button(self.master, text = "Move", command = lambda : self.prepare_execute_moves())
+        button_window = self.canvas.create_window(235, 20,window=self.button)
+
+        self.scrambleit = Button(self.master, text = "Scramble", command = lambda : self.scramble())
+        solveit_window = self.canvas.create_window(293, 20,window=self.scrambleit)
+          
+        #CUBE INIT
+        self.cube = Cube()
+
+        self.drawCube()
+
+    def drawCube(self):
+        colours = {'w': 'white', 'y' : 'yellow', 'r' : 'red', 'o' : 'orange', 'b' : 'blue', 'g' : 'green'}
+        self.master.title("Rubik's Cube by William Lambern")
+        canvas = self.canvas
+        canvas.delete('polygon')
+        c = self.cube
+        colours[c.sorted['-1-11'].colour[2]]
+        #right side
+        x = 200
+        y = 400
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-1-11'].colour[2]])
+        x = 250
+        y = 375
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['0-11'].colour[2]])
+        x = 300
+        y = 350
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['1-11'].colour[2]])
+        
+        x = 200
+        y = 350
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-101'].colour[2]])
+        x = 250
+        y = 325
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['001'].colour[2]])
+        x = 300
+        y = 300
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['101'].colour[2]])
+
+        x = 200
+        y = 300
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-111'].colour[2]])
+        x = 250
+        y = 275
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['011'].colour[2]])
+        x = 300
+        y = 250
+        canvas.create_polygon(x,y,x+50,y-25,x+50,y-75,x,y-50,outline="black", fill=colours[c.sorted['111'].colour[2]])
+
+        #left side
+        x = 200
+        y = 400
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-1-11'].colour[0]])
+        x = 150
+        y = 375
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-1-10'].colour[0]])
+        x = 100
+        y = 350
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-1-1-1'].colour[0]])
+        
+        x = 200
+        y = 350
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-101'].colour[0]])
+        x = 150
+        y = 325
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-100'].colour[0]])
+        x = 100
+        y = 300
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-10-1'].colour[0]])
+        
+        x = 200
+        y = 300
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-111'].colour[0]])
+        x = 150
+        y = 275
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-110'].colour[0]])
+        x = 100
+        y = 250
+        canvas.create_polygon(x,y,x-50,y-25,x-50,y-75,x,y-50,outline="black", fill=colours[c.sorted['-11-1'].colour[0]])
+
+        #top side
+        x = 200
+        y = 250
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['-111'].colour[1]])
+        x = 250
+        y = 225
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['011'].colour[1]])
+        x = 300
+        y = 200
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['111'].colour[1]])
+
+        x = 150
+        y = 225
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['-110'].colour[1]])
+        x = 200
+        y = 200
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['010'].colour[1]])
+        x = 250
+        y = 175
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['110'].colour[1]])
+
+        x = 100
+        y = 200
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['-11-1'].colour[1]])
+        x = 150
+        y = 175
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['01-1'].colour[1]])
+        x = 200
+        y = 150
+        canvas.create_polygon(x,y,x-50,y-25,x,y-50,x+50,y-25,outline="black", fill=colours[c.sorted['11-1'].colour[1]])
+        
+        
+        canvas.pack(fill=BOTH, expand=1)
+
+    def prepare_execute_moves(self):
+        
+        self.moves = self.e1.get().split(' ')
+        self.execute_moves(self.moves)
+
+    def execute_moves(self, moves):
+        if len(moves) > 0:
+            self.cube.rotate(moves[0])
+            self.drawCube()
+            self.moves.pop(0)
+            self.master.after(250, lambda : self.execute_moves(self.moves))
+
+    def scramble(self): #function here as we wan't to render after every move
+        options = ['f', 'r' , 'u' , 'l' , 'd' , 'b', "f'", "r'", "u'", "l'", "d'", "b'"]
+        moves = []
+        for i in range(5):
+            moves.append(random.choice(options))
+        self.moves = moves
+        self.execute_moves(moves)
+
+    
+
+root = Tk()
+Gui = Window(root)
+root.mainloop
+
+
